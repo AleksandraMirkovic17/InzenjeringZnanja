5033738213d78f11f2b7a30784205bb5aa89bc16
AleksandraMirkovic17 <mirkovicaleksandra@outlook.com>
2022-08-09 20:12:47 +0200

    fix: ontology fix

--- InzenjeringBACK/src/main/java/com/example/demo/InzenjeringBackApplication.java
+++ InzenjeringBACK/src/main/java/com/example/demo/InzenjeringBackApplication.java
@@ -1,13 +1,40 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.demo.services.OntologyServices;
 
 @SpringBootApplication
-public class InzenjeringBackApplication {
+public class InzenjeringBackApplication implements CommandLineRunner {
+	@Autowired
+	public OntologyServices service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(InzenjeringBackApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("********************************************************************************");
+		System.out.println("********************************************************************************");
+		System.out.println("********************************************************************************");
+		service.testOntology();
+
+	}
+
+	private String getQuery() {
+		return "prefix : <http://www.semanticweb.org/darko/ontologies/2022/6/untitled-ontology-7#>\n" +
+				"prefix owl: <http://www.w3.org/2002/07/owl#>\n" +
+				"prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+				"prefix xml: <http://www.w3.org/XML/1998/namespace>\n" +
+				"prefix xsd: <http://www.w3.org/2001/XMLSchema#>\n" +
+				"prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+				"SELECT ?x ?y ?z \n" +
+				"WHERE {\n" +
+				" ?x ?y ?z\n" +
+				"}\n";
 	}
 
 }
---
+++ InzenjeringBACK/src/main/java/com/example/demo/services/OntologyServices.java
@@ -0,0 +1,92 @@
+package com.example.demo.services;
+
+import org.apache.jena.base.Sys;
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.springframework.asm.TypeReference;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class OntologyServices {
+    private OWLOntologyManager manager;
+    private OWLOntology ontology;
+    private Model model;
+
+    public OntologyServices() {
+        createManager();
+        loadOntology();
+        loadModel();
+    }
+
+    public OWLOntologyManager getManager() {
+        return this.manager;
+    }
+
+    public OWLOntology getOntology() {
+        return this.ontology;
+    }
+
+    public Model getModel() {
+        return this.model;
+    }
+
+    public void testOntology() {
+        System.out.println(this.ontology);
+    }
+
+    public void executeTestQuery(String query) {
+        QueryExecution qe = QueryExecutionFactory.create(QueryFactory.create(query), model);
+        ResultSet rs = qe.execSelect();
+        while (rs.hasNext()) {
+            QuerySolution qs = rs.nextSolution();
+            System.out.println(qs.toString());
+        }
+    }
+
+    public List<QuerySolution> executeQuery(String query) {
+        List<QuerySolution> retVal = new ArrayList<>();
+        QueryExecution qe = QueryExecutionFactory.create(QueryFactory.create(query), model);
+        ResultSet rs = qe.execSelect();
+        while (rs.hasNext()) {
+            retVal.add(rs.nextSolution());
+        }
+        return retVal;
+    }
+
+    private void createManager() {
+        this.manager = OWLManager.createOWLOntologyManager();
+    }
+
+    private void loadOntology() {
+        String filePath = "..\\ontology\\classesAndInstances.owl";
+        File file = new File(filePath);
+        try {
+            this.ontology = this.manager.loadOntologyFromOntologyDocument(file);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private void loadModel() {
+        String filePath = "..\\ontology\\classesAndInstances.owl";
+        this.model = ModelFactory.createDefaultModel();
+        try {
+            InputStream is = new FileInputStream(filePath);
+            RDFDataMgr.read(this.model, is, Lang.TURTLE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
